b37d712cde1b55efb363fbc433a8ada0e7a28373
913165 <tinumistry@gmail.com>
2023-12-03 15:32:20 +0530

    dockerfile setting changed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import fastapi
 from student_data import students
+import os
 
 app = fastapi.FastAPI()
 
@@ -55,3 +56,8 @@ async def delete_student(student_id: int):
 # uvicorn main:app --reload --port 8080
 # command to create requirements.txt file
 # pip freeze > requirements.txt
+
+if __name__ == "__main__":
+    import uvicorn
+    server_port = int(os.environ.get('PORT', 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=server_port, log_level="info")
