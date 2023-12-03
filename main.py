89b290906bec22285ef05da6b1cf5ba220d16823
913165 <tinumistry@gmail.com>
2023-12-03 18:48:41 +0530

    new branch created

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ async def delete_student(student_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    server_port = int(os.environ.get('PORT', 8000))
+    server_port = int(os.environ.get('PORT', 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=server_port, log_level="info")
